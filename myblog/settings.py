719d20cb438ac801fcffddea805a88c7dbdedb9c
José Mendoza <sagaka1g@gmail.com>
2025-09-24 13:43:16 -0500

    added

--- myblog/settings.py
+++ myblog/settings.py
@@ -28,12 +28,14 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'web-production-47d8.up.railway.app',
 
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:8000',
     'http://127.0.0.1:8000',
+    'web-production-47d8.up.railway.app',
 ]
 
 
