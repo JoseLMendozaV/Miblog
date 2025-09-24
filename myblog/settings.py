e39a0dab006d5721bc970f9b382498a4ae1dd6af
José Mendoza <sagaka1g@gmail.com>
2025-09-24 14:01:36 -0500

    added

--- myblog/settings.py
+++ myblog/settings.py
@@ -144,6 +144,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 WHITENOISE_AUTOREFRESH = True
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
